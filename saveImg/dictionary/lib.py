6c819f1098522634cececcd5d04263910ab4df0a
To106kit <t.to106ki@gmail.com>
2022-09-24 00:59:38 +0900

    feat: 田村追加

--- saveImg/dictionary/lib.py
+++ saveImg/dictionary/lib.py
@@ -7,6 +7,7 @@ mydict= {
             # 4期生\
             "掛橋沙耶香":["KakehashiSayaka","https://nogiboard.blog.jp/archives/cat_169595.html"],
             "佐藤璃果":["SatoRika","https://nogiboard.blog.jp/archives/cat_169603.html"],
+            "田村真佑":["TamuraMayu","https://nogiboard.blog.jp/archives/cat_169599.html"],
             "弓木奈於":["YumikiNao","https://nogiboard.blog.jp/archives/cat_169607.html"],
             "林瑠奈":["HayashiRuna","https://nogiboard.blog.jp/archives/cat_169605.html"],
             # 5期生\
